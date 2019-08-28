65bde913fee615152540716b2d2328d3356959c1
Rick Ortiz <rick@Ricks-MacBook-Pro.local>
2019-08-27 23:05:06 -0400

    reformatted to use hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import Slider from './components/slider'
+import Colors from './components/slider'
 
 class App extends Component {
   render() {
-    return <Slider />
+    return <Colors />
   }
 }
 
--- src/components/slider.js
+++ src/components/slider.js
@@ -1,82 +1,62 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Slider extends Component {
-  state = {
-    hue: Math.ceil(Math.random() * 360),
-    saturation: Math.ceil(Math.ceil(Math.random() * 100)),
-    lightness: Math.ceil(Math.ceil(Math.random() * 100))
+const Colors = () => {
+  const [hue, setHue] = useState(Math.ceil(Math.random() * 360))
+  const [saturation, setSaturation] = useState(Math.ceil(Math.random() * 100))
+  const [lightness, setLightness] = useState(Math.ceil(Math.random() * 100))
+
+  const randomColor = () => {
+    setHue(Math.ceil(Math.random() * 360))
+    setSaturation(Math.ceil(Math.ceil(Math.random() * 100)))
+    setLightness(Math.ceil(Math.ceil(Math.random() * 100)))
   }
 
-  changeHue = event => {
-    this.setState({
-      hue: event.target.value
-    })
-  }
-
-  changeSaturation = event => {
-    this.setState({
-      saturation: event.target.value
-    })
-  }
-
-  changeLightness = event => {
-    this.setState({
-      lightness: event.target.value
-    })
-  }
-
-  randomColor = () => {
-    this.setState({
-      hue: Math.ceil(Math.random() * 360),
-      saturation: Math.ceil(Math.ceil(Math.random() * 100)),
-      lightness: Math.ceil(Math.ceil(Math.random() * 100))
-    })
-  }
-
-  render() {
-    return (
-      <section>
-        <div
-          className="color-picker"
-          style={{
-            backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${
-              this.state.lightness
-            }%)`,
-            fontSize: '1rem'
+  return (
+    <section>
+      <div
+        className="color-picker"
+        style={{
+          backgroundColor: `hsl(${hue},${saturation}%,${lightness}%)`,
+          fontSize: '1rem'
+        }}
+      >
+        <h1 className="hsl-slider"> Color picker </h1>
+        <input
+          className="hue-slider"
+          type="range"
+          max="100"
+          min="0"
+          value={hue}
+          onChange={e => {
+            setHue(e.target.value)
           }}
-        >
-          <h1 className="hsl-slider"> Color picker </h1>
-          <input
-            className="hue-slider"
-            type="range"
-            max="100"
-            min="0"
-            value={this.state.value}
-            onChange={this.changeHue}
-          />
-          <h3>Hue</h3>
-          <input
-            className="saturation-slider"
-            type="range"
-            max="100"
-            min="0"
-            value={this.state.value}
-            onChange={this.changeSaturation}
-          />
-          <h3>Saturation</h3>
-          <input
-            className="lightness-slider"
-            type="range"
-            max="100"
-            min="0"
-            value={this.state.value}
-            onChange={this.changeLightness}
-          />
-        </div>
+        />
+        <h3>{hue}</h3>
+        <input
+          className="saturation-slider"
+          type="range"
+          max="100"
+          min="0"
+          value={saturation}
+          onChange={e => {
+            setSaturation(e.target.value)
+          }}
+        />
+        <h3>{saturation}</h3>
+        <input
+          className="lightness-slider"
+          type="range"
+          max="100"
+          min="0"
+          value={lightness}
+          onChange={e => {
+            setLightness(e.target.value)
+          }}
+        />
         <h3>Lightness</h3>
-      </section>
-    )
-  }
+      </div>
+    </section>
+  )
 }
 
-export default Slider
+export default Colors
